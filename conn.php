9829d35ba0cc8a9dabe2a17aaf70cadef63be1e9
Erin Hardnett <aliah17579@gmail.com>
2020-11-23 12:40:02 -0600

    pushing new changes

--- conn.php
+++ conn.php
@@ -3,7 +3,7 @@
 // Use with Homebrew terminal connection
 $dbhost = '127.0.0.1'; // localhost
 $dbuname = 'root';
-$dbpass = 'Cheetah07!';
+$dbpass = '';
 $dbname = 'accidentdb';
 
 // Use with MAMP connection
